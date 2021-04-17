45e7d46ca1dda61f0f173dab8863795d61692d3a
unknown <zim21den21@gmail.com>
2021-04-17 19:39:02 +0300

    config

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://to-do-list:yourpasword@cluster0.g06c0.mongodb.net/Cluster0?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const expres = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+
+const app = expres();
+
+//Bodyparser Middleware
+app.use(bodyParser.json());
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//Connect to Mongo DB
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongo connected..."))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`server work on port ${port}`));
